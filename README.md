f712663b9a19c4dce81a3b754ca0f7a26ea4d59a
Roman Stigler <fromanx@gmail.com>
2023-02-13 17:22:18 -0600

    Finalized functionality of project

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# Module-8-Challenge
+#Employee Team Generator
+
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)(https://opensource.org/licenses/MIT)
+
+# Description
+
+A generator used to add employees into a website for easy viewing.
+
+# Table of Contents
+
+*   [Installation](#Installation)
+
+*   [Usage](#Usage)
+
+*   [License](#License)
+
+*   [Contribution](#Contribution)
+
+*   [Tests](#Tests)
+
+*   [Questions](#Questions)
+
+## Installation
+
+To install necessary dependicies, run the following command:
+-------
+npm i
+-------
+
+## Usage
+
+N/A
+
+## License
+
+This project is licensed under the MIT license.
+
+## Contributing
+
+N/A
+
+## Tests
+
+To run tests, run the following command:
+------
+npm test
+------
+
+## Questions
+
+If you have any questions regarding this repo, open an issue or contact me directly at fromanX@gmail.com. You can find more of my work at [romX10](https://github.com/romX10).
--- index.js
+++ index.js
@@ -4,37 +4,87 @@ const Employee = require('./lib/employee');
 const Engineer = require('./lib/engineer');
 const Manager = require('./lib/manager');
 const Intern = require('./lib/intern');
-var workers = [];
+
 const generateEmployee = ({name, id, email}) =>
 `
-
-<h1> Employee : ${name}</h1>
-<h2>Id : ${id}</h2>
-<h3>Email : ${email}</h3>
+        <div class="card" style="width: 18rem;">
+            <div class="card-header bg-primary text-white">
+                <h5>${name}</h5>
+                <h5><i class="fa-solid fa-user"></i> Employee</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><i class="fa-solid fa-circle-info"></i> ID: ${id}</li>
+                <li class="list-group-item"><i class="fa-solid fa-envelope"></i> Email: <a>${email}</a></li>
+            </ul>
+        </div>
 `;
 const generateEngineer = ({name, id, email, github}) =>
 `
-<h1>Engineer : ${name}</h1>
-<h2>Id : ${id}</h2>
-<h3>Email : ${email}</h3>
-<h4>Github username : ${github}</h4>
+        <div class="card" style="width: 18rem;">
+            <div class="card-header bg-primary text-white">
+                <h5>${name}</h5>
+                <h5><i class="fa-solid fa-gear"></i> Engineer</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><i class="fa-solid fa-circle-info"></i> ID: ${id}</li>
+                <li class="list-group-item"><i class="fa-solid fa-envelope"></i> Email: ${email}</li>
+                <li class="list-group-item"><i class="fa-brands fa-github"></i> Github: <a href="github.com/${github}">${github}</a></li>
+            </ul>
+        </div>
 `;
 const generateManager = ({name, id, email, officeNumber}) =>
 `
-<h1>${name}</h1>
-<h2>${id}</h2>
-<h3>${email}</h3>
-<h4>${officeNumber}</h4>
+        <div class="card" style="width: 18rem;">
+            <div class="card-header bg-primary text-white">
+                <h5>${name}</h5>
+                <h5><i class="fa-solid fa-suitcase"></i> Manager</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><i class="fa-solid fa-circle-info"></i> ID: ${id}</li>
+                <li class="list-group-item"><i class="fa-solid fa-envelope"></i> Email: <a>${email}</a></li>
+                <li class="list-group-item"><i class="fa-solid fa-building"></i> Office Number: ${officeNumber}</li>
+            </ul>
+        </div>
 `;
 const generateIntern = ({name, id, email, school}) =>
 `
-<h1>${name}</h1>
-<h2>${id}</h2>
-<h3>${email}</h3>
-<h4>${school}</h4>
+        <div class="card" style="width: 18rem;">
+            <div class="card-header bg-primary text-white">
+                <h5>${name}</h5>
+                <h5><i class="fa-solid fa-graduation-cap"></i> Intern</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><i class="fa-solid fa-circle-info"></i> ID: ${id}</li>
+                <li class="list-group-item"><i class="fa-solid fa-envelope"></i> Email: <a>${email}</a></li>
+                <li class="list-group-item"><i class="fa-solid fa-school-flag"></i> School: ${school}</li>
+            </ul>
+        </div>
 `;
+const startTags = `<htmlNEW TEXT lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>Employee List</title>
+    <script src="https://kit.fontawesome.com/b8ecb5e766.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav class="navbar bg-danger">
+        <div class="container-fluid">
+          <a class="navbar-brand text-white mx-auto" href="#">My Team</a>
+        </div>
+    </nav>
+    <div class="card-group">`
+const endingTags = `</div> </body> </html>`;
 
+function start() {
+    fs.appendFile('index.html', startTags, (error) =>
+                    error ? console.log(error) : console.log(' ')
+                    );
+}
 function init() {
+    console.log('');
     inquirer.prompt([
         {
             type: 'list',
@@ -45,7 +95,24 @@ function init() {
     ])
     .then((answers) => {
         if (answers.initialize === 'No') {
-            return
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'done',
+                    message: 'Are you done adding employees?',
+                    choices: ['Yes', 'No']
+                }
+            ])
+            .then((answers) => {
+                if (answers.done === 'Yes') {
+                    fs.appendFile('index.html', endingTags, (error) =>
+                    error ? console.log(error) : console.log('HTML Successfully generated!')
+                    );
+                }
+                else {
+                    init();
+                }
+            })
         } 
         else {
             inquirer.prompt([
@@ -79,8 +146,9 @@ function init() {
                         const newEmployee = generateEmployee(answers);
 
                         fs.appendFile('index.html', newEmployee, (error) =>
-                          error ? console.log(error) : console.log('Employee successfully added!')
+                        error ? console.log(error) : console.log('Employee successfully added!')
                         );
+                        init();
                     });
                 }
                 else if (answers.jobType === 'Engineer') {
@@ -110,8 +178,9 @@ function init() {
                         const newEngineer = generateEngineer(answers);
 
                         fs.appendFile('index.html', newEngineer, (error) =>
-                          error ? console.log(error) : console.log('Engineer successfully added!')
+                        error ? console.log(error) : console.log('Engineer successfully added!')
                         );
+                        init();
                     });
                 }
                 else if (answers.jobType === 'Manager') {
@@ -141,8 +210,10 @@ function init() {
                         const newManager = generateManager(answers);
 
                         fs.appendFile('index.html', newManager, (error) =>
-                          error ? console.log(error) : console.log('Manager successfully added!')
+                        error ? console.log(error) : console.log('Manager successfully added!')
                         );
+                        init();
+                       
                     });
 
                 }
@@ -173,13 +244,16 @@ function init() {
                         const newIntern = generateIntern(answers);
 
                         fs.appendFile('index.html', newIntern, (error) =>
-                          error ? console.log(error) : console.log('Intern successfully added!')
+                        error ? console.log(error) : console.log('Intern successfully added!')
                         );
+                        init();
                     });
                 }
             })
         }
     })
 }
+
+start();
 init();
 
